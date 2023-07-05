6077889bca88def8b7e127ca0ab8154c33df3f4c
mansishrivastav <mansishrivastav04@gmail.com>
2023-07-06 00:12:48 +0530

    weather project

--- src/components/EnvironmentalConditions.js
+++ src/components/EnvironmentalConditions.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import './EnvironmentalConditions.css'
+import React from "react";
+import "./EnvironmentalConditions.css";
 
-const EnvironmentalConditions = ({data}) => {
+const EnvironmentalConditions = ({ data }) => {
   return (
     <>
       <section className="bottom">
-          <div className="feels">
-            {data.main?<p className="bold">{data.main.feels_like.toFixed()}°F</p>:null}
-            <p>Feels-like</p>
-          </div>
-          <div className="humidity">
-            {data.main?<p className="bold">{data.main.humidity}%</p>:null}
-            <p>Humidity</p>
-          </div>
-          <div className="wind-speed">
-           {data.wind?<p className="bold">{data.wind.speed.toFixed()}MPH</p>:null}
-            <p>Wind-speed</p>
-          </div>
-        </section>
+        <div className="feels">
+          {data.main ? (
+            <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+          ) : null}
+          <p>Feels-like</p>
+        </div>
+        <div className="humidity">
+          {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+          <p>Humidity</p>
+        </div>
+        <div className="wind-speed">
+          {data.wind ? (
+            <p className="bold">{data.wind.speed.toFixed()}MPH</p>
+          ) : null}
+          <p>Wind-speed</p>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default EnvironmentalConditions
+export default EnvironmentalConditions;
